fc96196a5fe82d2312371e753189597438f2a609
Peter Bush <poytebush@gmail.com>
2017-11-25 15:41:41 +0000

    Added Model loading files

---
+++ ModelLoad.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ModelLoad {
+    public vector3 SpawnLocation;
+    public int HeightOffset;
+    public int WidthOffset;
+}
---
+++ ModelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelLoader : MonoBehaviour {
+
+    public List<ModelLoad> modelLoad;
+
+
+	// Use this for initialization
+	void Start () {
+        modelLoad = new List<ModelLoad>();
+
+        modelLoad.AddRange(LoadHealthPotions("ModelLoad.json"));
+
+    }
+
+    private List<Pickup> LoadHealthPotions(string PickupPath)
+    {
+        string Path = Application.dataPath + "/Scripts/Pickups/" + PickupPath;
+        string[] MapData = FileIOHelper.FileDataToArray(Path);
+        string JSONString = string.Join("", MapData);
+
+        List<Pickup> output = new List<Pickup>();
+        output.Add(JsonUtility.FromJson<Pickup>(JSONString));
+
+        return output;
+    }
+}
